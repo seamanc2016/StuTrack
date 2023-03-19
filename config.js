271f31f2a03283479eadf281734850ff363c3ac2
seamanc2016 <seamanc2016@fau.edu>
2023-03-19 15:56:02 -0400

    We need config in repo for testing.

---
+++ config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.app = {};
+config.server = {};
+config.db = {};
+
+//server vars
+config.server.port = 5678;
+
+//MongoDB creds
+config.db.host = 'cluster0.awaxvxj.mongodb.net';
+config.db.port = 27071;
+config.db.user = 'seamanc2016';
+config.db.pass = '4gtUJX0NqzylRnwg';
+config.db.name = 'college';
+config.db.collection = 'students';
+
+module.exports = config;
